23e63f15aac78d70b548f9b40c0fdfc522d0355c
Linn Johansen <linn.es.johansen@gmail.com>
2025-01-31 14:53:20 -0800

    Setup step 1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB Connected'))
+  .catch((err) => console.error('❌ Connection Failed', err));
+
+// Start server
+const app = require('./app');
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`🤖 Server running on port ${port}`));
