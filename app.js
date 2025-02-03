9109cca658cbdb9517099dc7ab707dfde36e8035
Linn Johansen <linn.es.johansen@gmail.com>
2025-02-03 15:04:07 -0800

    CORS updates

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // app.js
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://shop-morningstar.netlify.app',
+];
+
 const express = require('express');
 const cors = require('cors');
 
@@ -7,7 +12,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'https://shop-morningstar.netlify.app' }));
+app.use(
+  cors({
+    origin: (origin, callback) =>
+      !origin || allowedOrigins.includes(origin)
+        ? callback(null, true)
+        : callback(new Error('CORS policy does not allow this origin!')),
+  })
+);
 
 // Routes
 const productRouter = require('./routes/productRoutes');
